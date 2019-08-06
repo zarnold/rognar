3d0c57fe43c313841466b01a3fd728551136b5b7
DESKTOP-008IK9K\arnol <arnold.zephir@prevision.io>
2019-08-06 15:57:53 +0200

    Use IIF for buildig private method that does not link the scope

--- modules/dialogLib.js
+++ modules/dialogLib.js
@@ -6,81 +6,102 @@
 
 "use strict";
 
-export default class Writer {
+// Code change to make method private but without sharing the scope 
+// Use immediately invocated function
 
-    constructor(dialogWindowId) {
-        let target = document.getElementById(dialogWindowId);
-        
-        // Properties 
+let Writer = (function () {
 
-        this.dialogTree= {};
-        this.alreadyReadChoices=[];
-        this.currentTalk={};
+    // Convention is to use a lodash for private method
+    // Expect the loading function to be asynchronous     
+    const _loadDialogTree = function(path) {
 
-
-        if(target) {
-            this.target = target;
-            
-            this.target.classList.add("ff7");
-
-            //Header with creature speech
-            let creatureSpeechContainer = document.createElement("h2"); 
-            creatureSpeechContainer.setAttribute("id","creature-speech");
-            this.target.appendChild(creatureSpeechContainer);
-
-            // The list of choice
-            let playerChoiceContainer = document.createElement("ul"); 
-            playerChoiceContainer.setAttribute("id","player-choice");
-            this.target.appendChild(playerChoiceContainer);
-
-        } else {
-            console.error("The dialog Window Id you provided is not available");
-        }
-    }
-
-    loadDialogTree(url) {
-        // Load it the convenient way (read a file, a distant url, etc...)
-        this.dialogTree = url;
-        this.alreadyReadChoices=[];
-        
-    }
-
-
-    update(speech, choices) {
-        
-
-        // Get the Creature speech element and the player choice list
-        let hdr = this.target.querySelector("#creature-speech");
-        let chc = this.target.querySelector("#player-choice");  
-        let  newContent;
-
-        // First empty them
-        // faster way seems to clone the node without cloning its children
-        
-        let cNode ;
-        cNode = hdr.cloneNode(false);     
-        hdr.parentNode.replaceChild(cNode ,hdr);
-        cNode = chc.cloneNode(false);    
-        chc.parentNode.replaceChild(cNode ,chc);
-
-
-        // Then add the speech
-        // do not forget to get the reference again as it has changed when cloning
-        hdr = this.target.querySelector("#creature-speech");
-        newContent= document.createTextNode(speech); 
-        hdr.appendChild(newContent);  
-
-        chc = this.target.querySelector("#player-choice");  
-
-        // Add an li for each options
-        choices.forEach(function(el) {
-            let l = document.createElement('li');        
-            newContent= document.createTextNode(el); 
-            l.appendChild(newContent);
-            chc.appendChild(l);  
+        return new Promise( function(res, rej) {
+            res(4);
         });
-        
+    }
+
+    class Writer {
+
+        constructor(dialogWindowId) {
+            let target = document.getElementById(dialogWindowId);
+
+            // Properties 
+
+            this.dialogTree = {};
+            this.alreadyReadChoices = [];
+            this.currentTalk = {};
+
+
+            if (target) {
+                this.target = target;
+
+                this.target.classList.add("ff7");
+
+                //Header with creature speech
+                let creatureSpeechContainer = document.createElement("h2");
+                creatureSpeechContainer.setAttribute("id", "creature-speech");
+                this.target.appendChild(creatureSpeechContainer);
+
+                // The list of choice
+                let playerChoiceContainer = document.createElement("ul");
+                playerChoiceContainer.setAttribute("id", "player-choice");
+                this.target.appendChild(playerChoiceContainer);
+
+            } else {
+                console.error("The dialog Window Id you provided is not available");
+            }
+        }
+
+
+        update(speech, choices) {
+
+
+            // Get the Creature speech element and the player choice list
+            let hdr = this.target.querySelector("#creature-speech");
+            let chc = this.target.querySelector("#player-choice");
+            let newContent;
+
+            // First empty them
+            // faster way seems to clone the node without cloning its children
+
+            let cNode;
+            cNode = hdr.cloneNode(false);
+            hdr.parentNode.replaceChild(cNode, hdr);
+            cNode = chc.cloneNode(false);
+            chc.parentNode.replaceChild(cNode, chc);
+
+
+            // Then add the speech
+            // do not forget to get the reference again as it has changed when cloning
+            hdr = this.target.querySelector("#creature-speech");
+            newContent = document.createTextNode(speech);
+            hdr.appendChild(newContent);
+
+            chc = this.target.querySelector("#player-choice");
+
+            // Add an li for each options
+            choices.forEach(function (el) {
+                let l = document.createElement('li');
+                newContent = document.createTextNode(el);
+                l.appendChild(newContent);
+                chc.appendChild(l);
+            });
+
+        }
+
+        async runNewDialog(pathToDialogTree) {
+
+            // Reset 
+            this.alreadyReadChoices = [];
+            this.dialogTree = await _loadDialogTree(pathToDialogTree)
+            return 0;
+        }
+
+
     }
 
 
-}
+    return Writer
+})();
+
+export default Writer;
